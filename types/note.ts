5428dfae9a5963601e194e95e6c8ee6b9ab4eaaa
Tetiana <Korostijtatiana@gmail.com>
2025-09-14 13:25:29 +0300

    add

--- types/note.ts
+++ types/note.ts
@@ -8,7 +8,7 @@ export interface Note {
 }
 
 export type NoteId = Note["id"];
-export type Tag = Note["tag"];
+export type Tag = Note["tag"]; // існуючий тип
 export type SortBy = "title" | "createdAt" | "updatedAt";
 
 export const CATEGORIES = [
@@ -23,10 +23,23 @@ export const CATEGORIES = [
 export type Category = (typeof CATEGORIES)[number];
 export type CategoryNoAll = Exclude<Category, "All">;
 
+// ✅ додай визначення NoteTag
 export type NoteTag = Tag;
 
 export interface NewNoteData {
   title: string;
   content?: string;
-  tag: NoteTag;
+  tag: NoteTag; // тепер тип існує
+}
+
+export const enum Routes {
+  Home = "/",
+  NoteDetails = "/notes/",
+  NotesFilter = `/notes/filter/`,
+  NoteAction = "/notes/action/",
+  NoteCreate = `${NoteAction}create/`,
+  SignIn = "/sign-in",
+  SignUp = "/sign-up",
+  Profile = "/profile",
+  ProfileEdit = `${Profile}/edit`,
 }
